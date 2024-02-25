d621e1ff8bd1b8471bba26b02e78efdbd858963e
Nyuhei <nyuhei06@gmail.com>
2024-02-25 18:38:00 +0900

    伝統経済

--- common/scripted_effects/seat.py
+++ common/scripted_effects/seat.py
@@ -2,7 +2,7 @@ import math
 f1 = "add_to_array = { array = GER_seat_x_position value ="
 f2 = " }\nadd_to_array = { array = GER_seat_y_position value ="
 f3 = " }\n"
-with open("common/scripted_effects/seat.txt", "w") as f:
+with open("common/scripted_effects/seat", "w") as f:
     for theta in range (0,41):
         for r in range (0,12):
            #z = round"切り捨て"("１アイコンの大きさ"*(r"現在の内側からの距離"+"内側の空白")*math.sin"任意の三角関数"(角度[rad]),3"小数点以下の桁")
